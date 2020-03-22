d2702e9ce896422ba3cbfca315dbd6ec4150da29
sajicode <sajioloye@gmail.com>
2020-03-22 02:33:20 +0100

    create faq & notfound pages

--- main.go
+++ main.go
@@ -17,9 +17,22 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:sajioloye@gmail.com\">support@lenslocked.com</a>.")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "What questions do you have? Share them here and we would do our best to answer. :)")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "Sorry, we couldn't get the page you requested")
+}
+
 func main() {
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":4500", r)
 }
