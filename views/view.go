7ca09d21d05bc0b9d143c6a826da0656d96e60b0
sajicode <sajioloye@gmail.com>
2020-03-22 16:53:03 +0100

    create & use bootstrap layout

--- views/view.go
+++ views/view.go
@@ -3,8 +3,8 @@ package views
 import "html/template"
 
 // NewView handles parsing of our templates
-func NewView(files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml")
+func NewView(layout string, files ...string) *View {
+	files = append(files, "views/layouts/bootstrap.gohtml", "views/layouts/footer.gohtml")
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -13,10 +13,12 @@ func NewView(files ...string) *View {
 
 	return &View{
 		Template: t,
+		Layout:   layout,
 	}
 }
 
 // View struct for template view
 type View struct {
 	Template *template.Template
+	Layout   string
 }
