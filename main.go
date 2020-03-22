7a3056b1f1685b279ca600646f89405aa20d904f
sajicode <sajioloye@gmail.com>
2020-03-22 01:22:47 +0100

    add contact page

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type": "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my cool site</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my cool site</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:sajioloye@gmail.com\">support@lenslocked.com</a>.")
+	}
 }
 
 func main() {
