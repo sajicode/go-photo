f65f7138b80844e4604413b9b64dbce80602e386
sajicode <sajioloye@gmail.com>
2020-03-22 01:59:40 +0100

    use julien schmidt httprouter

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +19,14 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":4500", nil)
+	router := httprouter.New()
+	router.GET("/hello/:name", Hello)
+
+	// http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":4500", router)
 }
