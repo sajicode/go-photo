1f20c086b56498b82647fa4abd735de7c2aa9314
sajicode <sajioloye@gmail.com>
2020-03-22 03:23:14 +0100

    working with templates

---
+++ exp/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name  string
+	Dog   string
+	Int   int
+	Float float64
+	Slice []string
+	Map   map[string]string
+}
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	data := User{
+		Name:  "Jack Sparrow",
+		Dog:   "Bolt",
+		Float: 3.14,
+		Slice: []string{"a", "b", "c"},
+		Map: map[string]string{
+			"key1": "value1",
+			"key2": "value2",
+		},
+	}
+
+	err = t.Execute(os.Stdout, data)
+	if err != nil {
+		panic(err)
+	}
+}
