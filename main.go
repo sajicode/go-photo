35eba13d296bd695503d0852d73a7fcc0d585889
sajicode <sajioloye@gmail.com>
2020-03-22 01:33:34 +0100

    add a 404 page

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to my cool site</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:sajioloye@gmail.com\">support@lenslocked.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "<h1>We could not find the page you requested :(</h1><p>Please send an email if you keep getting sent to an invalid page.</p>")
 	}
 }
 
