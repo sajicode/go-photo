002730566dcc57f519fbc2d8039c6b9e2368d55a
sajicode <sajioloye@gmail.com>
2020-03-24 03:19:25 +0100

    setup create user method

--- models/users.go
+++ models/users.go
@@ -45,6 +45,11 @@ func (us *UserService) ByID(id uint) (*User, error) {
 	}
 }
 
+// Create will create the provided user data
+func (us *UserService) Create(user *User) error {
+	return us.db.Create(user).Error
+}
+
 // Close DB connection
 func (us *UserService) Close() error {
 	return us.db.Close()
